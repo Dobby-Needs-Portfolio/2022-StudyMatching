cdf4f5887beb3cbfa82ad697f73c071258e73ff5
Mina-1316 <mina1316.canreply@gmail.com>
2022-07-04 00:30:34 +0900

    project: initialize spring backend project
    백엔드/SSR 프론트 로직을 담당할 스프링 프로젝트 초기 소스 코드 추가

---
+++ backend/src/main/java/deu/ac/kr/webapp/StudyMatchingApplication.java
@@ -0,0 +1,13 @@
+package deu.ac.kr.webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyMatchingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyMatchingApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/deu/ac/kr/webapp/StudyMatchingApplicationTests.java
@@ -0,0 +1,13 @@
+package deu.ac.kr.webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyMatchingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
