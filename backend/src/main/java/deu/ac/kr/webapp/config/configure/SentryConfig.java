c200994f11ffb156a385e9e9758e6ea2d1f50ead
Mina-1316 <mina1316.canreply@gmail.com>
2022-07-22 12:08:24 +0900

    project: fix lint violation of previous files

--- backend/src/main/java/deu/ac/kr/webapp/config/configure/SentryConfig.java
+++ backend/src/main/java/deu/ac/kr/webapp/config/configure/SentryConfig.java
@@ -4,6 +4,9 @@ import io.sentry.spring.tracing.SentryTracingConfiguration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
+/**
+ * Class for Sentry service Configuration.
+ */
 @Import(SentryTracingConfiguration.class)
 @Configuration
 public class SentryConfig {
