2cbbe50d908c08211e3265f37fd814cb23d92452
Mina- <mina1316.canreply@gmail.com>
2022-07-22 19:10:14 +0900

    project: configure java linter that check code satisfies google convention
    * project: configure linter with google_check configurations
    
    * project: fix lint violation of previous files
    
    * docs: add convention configuration for intellij

--- backend/src/main/java/deu/ac/kr/webapp/StudyMatchingApplication.java
+++ backend/src/main/java/deu/ac/kr/webapp/StudyMatchingApplication.java
@@ -3,11 +3,12 @@ package deu.ac.kr.webapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Spring Boot Application Entry Point.
+ */
 @SpringBootApplication
 public class StudyMatchingApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(StudyMatchingApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(StudyMatchingApplication.class, args);
+  }
 }
