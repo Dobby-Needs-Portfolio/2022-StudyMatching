b8331101e63752fed7b977530d20058d06ba2566
Mina- <mina1316.canreply@gmail.com>
2022-08-10 04:52:02 +0900

    project: update gradlew version and delete integration test on unit test module

--- backend/src/test/java/deu/ac/kr/webapp/StudyMatchingApplicationTests.java
+++ backend/src/test/java/deu/ac/kr/webapp/StudyMatchingApplicationTests.java
@@ -3,11 +3,10 @@ package deu.ac.kr.webapp;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class StudyMatchingApplicationTests {
-
   @Test
   void contextLoads() {
+    assert (true);
   }
 
 }
